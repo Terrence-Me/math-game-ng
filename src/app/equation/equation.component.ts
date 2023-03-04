cbb874326afd14fde24f20cbf7a8d0f58a331f21
Terrence Meikle <terrence_meikle@yahoo.com>
2023-03-03 19:51:05 -0500

    extract validation logic

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { MathValidators } from '../math-validators';
 
 @Component({
   selector: 'app-equation',
@@ -13,16 +15,7 @@ export class EquationComponent {
       b: new FormControl(this.randomNumber()),
       answer: new FormControl(''),
     },
-    [
-      (form: AbstractControl) => {
-        const { a, b, answer } = form.value;
-        if (a + b === parseInt(answer)) {
-          return null;
-        }
-
-        return { addition: true };
-      },
-    ]
+    [MathValidators.addition]
   );
 
   get a() {
