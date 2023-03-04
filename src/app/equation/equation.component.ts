84a7c4465c97e1e627d231981568eeaa1eb210e1
Terrence Meikle <terrence_meikle@yahoo.com>
2023-03-03 19:15:54 -0500

    add values to form

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.css']
+  styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl(''),
+  });
 
+  get a() {
+    return this.mathForm.value.a;
+  }
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
